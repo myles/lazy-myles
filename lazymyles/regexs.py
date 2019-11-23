d19eb41b37df820287893895df434ce32ed2572a
Myles Braithwaite <me@mylesbraithwaite.com>
2019-11-22 22:03:12 -0500

    :sparkles: Add some regexes.

---
+++ lazymyles/regexs.py
@@ -0,0 +1,8 @@
+import re
+
+EMAIL_REGEX = re.compile(
+    r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+)
+URL_REGEX = re.compile(
+    r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+)
---
+++ tests/test_regexs.py
@@ -0,0 +1,28 @@
+import string
+
+import pytest
+
+from lazymyles import regexs
+
+
+@pytest.mark.parametrize(
+    'email',
+    (
+        'me@mylesbraithwaite.org',
+        'me+lazy-myles@mylesbraithwaite.org'
+    )
+)
+def test_email_regex(email):
+    assert regexs.EMAIL_REGEX.match(email)
+
+
+@pytest.mark.parametrize(
+    'url',
+    (
+        'https://mylesbraithwaite.org/',
+        'http://mylesbraithwaite.org/',
+        'http://www.mylesbraithwaite.org/',
+    )
+)
+def test_url_regex(url):
+    assert regexs.URL_REGEX.match(url)
