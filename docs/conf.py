9928e22ff6639508058f6e28d85c570a97be4431
Myles Braithwaite <me@mylesbraithwaite.com>
2022-12-17 21:18:48 -0500

    📝 ENG-15 — Setup docs and website for lazy-myles (#2)
    * :heavy_plus_sign: Added Sphinx package.
    
    * :memo: Stated working on the docs.
    
    * :construction: Did some stuff.

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import sys
+
+ROOT_PATH = Path(__file__).resolve().parent.parent
+
+sys.path.insert(0, str(ROOT_PATH))
+
+import lazymyles
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'lazy-myles'
+copyright = '2022, Myles Braithwaite'
+author = 'Myles Braithwaite'
+release = lazymyles.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'myst_parser',
+]
+
+source_suffix = {
+    '.md': 'markdown',
+    '.rst': 'restructuredtext',
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- lazymyles/__init__.py
+++ lazymyles/__init__.py
@@ -1,7 +1,9 @@
 from .download_file import download_file
 from .html.description_list import dl_to_dict
+from .__version__ import __version__
 
 __all__ = [
     "download_file",
     "dl_to_dict",
+    "__version__",
 ]
