fd3f95dc0970a6cbd8a97b5e3643d30fbc8dec13
Myles Braithwaite <me@mylesbraithwaite.com>
2022-12-17 22:44:52 -0500

    Update test_regexs.py

--- tests/test_regexs.py
+++ tests/test_regexs.py
@@ -7,16 +7,16 @@ from lazymyles import regexs
     "email", ("me@mylesbraithwaite.org", "me+lazy-myles@mylesbraithwaite.org")
 )
 def test_email_regex(email):
-    assert regexs.EMAIL_REGEX.match(email)
+    assert regexs.EMAIL_REGEX.match(email) is True
 
 
 @pytest.mark.parametrize(
     "http_url",
     (
-        "https://mylesbraithwaite.org/",
-        "http://mylesbraithwaite.org/",
-        "http://www.mylesbraithwaite.org/",
+        "https://lazy-myles.com/",
+        "http://lazy-myles.com/",
+        "http://www.lazy-myles.com/",
     ),
 )
 def test_http_url_regex(http_url):
-    assert regexs.HTTP_URL_REGEX.match(http_url)
+    assert regexs.HTTP_URL_REGEX.match(http_url) is True
